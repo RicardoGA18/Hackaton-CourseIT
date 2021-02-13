e03393833b034f1d6a0a952a1a7de219491ccb3b
YonJun <jhon_08_2007@hotmail.com>
2021-02-13 18:43:51 -0500

    add iconlocation

--- src/components/Animales/Header.jsx
+++ src/components/Animales/Header.jsx
@@ -1,12 +1,18 @@
 import React, { Fragment } from 'react'
+import LocationIcon from '../../assets/location_icon.svg';
 
 const Header = ({ name, location }) => {
 
     return <Fragment>
         <div className="animal__header__container">
             <h1 className="animal__header__name">{name}</h1>
-            <p className="animal__header__location">{location}</p>
 
+            <div className="animal__header__location">
+                <img src={LocationIcon} alt="icon-location" style={{ width: 25, height: 25 }} />
+                <p>
+                    {location}
+                </p>
+            </div>
         </div>
     </Fragment>
 
