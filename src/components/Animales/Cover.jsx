3509d0f8b40bb4fe8de3f301823cc12a3e4de5e1
YonJun <jhon_08_2007@hotmail.com>
2021-02-13 19:08:52 -0500

    add close icon

--- src/components/Animales/Cover.jsx
+++ src/components/Animales/Cover.jsx
@@ -1,11 +1,14 @@
 import React, { Fragment } from 'react'
+import CloseIcon from '../../assets/close_icon.svg'
 
 const Cover = ({ alt, src, onClose }) => {
 
     return <Fragment>
         <div className="animal__cover__container">
             <img className="animal__cover__image" alt={alt} src={src} />
-            <button className="animal__cover__closebtn" onClick={onClose}>x</button>
+            <button className="animal__cover__closebtn ripple" onClick={onClose}>
+                <img src={CloseIcon} alt="close-icon" />
+            </button>
         </div>
     </Fragment>
 
