9c446faf92c4eca998180f86a429d72290a2fc25
Ricardo Gambini <ricardo.gambini18@outlook.com>
2021-02-15 19:51:49 -0500

    pre-account

--- src/containers/Account.jsx
+++ src/containers/Account.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import '../css/Account.css'
+import backAccount from '../assets/photos/backAccount.png'
 
 const Account = () => {
   return (
     <div className="Account">
-      
+      <div className="Account__Head">
+        <img src={backAccount}/>
+        <p>Perfil</p>
+      </div>
     </div>
   )
 }
